1fb2c3471deef695d12fb17be14216873ae650a5
PRIYAM PATEL <patelpriyam79@gmail.com>
2024-03-18 20:35:44 -0400

    Initial Commit

---
+++ GPS (L7)Tests/GPS__L7_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  GPS__L7_Tests.swift
+//  GPS (L7)Tests
+//
+//  Created by PRIYAM PATEL on 18/03/24.
+//
+
+import XCTest
+@testable import GPS__L7_
+
+final class GPS__L7_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
